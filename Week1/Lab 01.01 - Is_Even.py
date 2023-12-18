fb8b8681db69c4c76d28c61e7d2801af0679296c
Babe <tonnum.kawin@gmail.com>
2023-12-18 08:48:01 +0700

    lol

--- Week1/Lab 01.01 - Is_Even.py
+++ Week1/Lab 01.01 - Is_Even.py
@@ -2,5 +2,6 @@
 
 def is_even(num):
     """Is Even no %"""
-    return True if int((num//2)*2) == num else False
-print(is_even(int(input())))
+    #return True if int((num//2)*2) == num else False
+    return True if num[-1] in "02468" else False
+print(is_even(str(input())))
