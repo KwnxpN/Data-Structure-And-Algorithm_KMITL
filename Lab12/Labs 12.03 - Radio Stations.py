d7ba0146d874ca7a5a973c3b073b705fa145bb43
KwnxpN <tonnum.kawin@gmail.com>
2024-02-29 20:49:34 +0700

    finished

--- Lab12/Labs 12.03 - Radio Stations.py
+++ Lab12/Labs 12.03 - Radio Stations.py
@@ -1,9 +1,19 @@
 """KKK"""
+from json import loads
 
-def find_station(stations):
-    """find the best person for that have stations we need"""
-
-def main():
-    """main"""
-
-main()
+def radio_stations(stations: list):
+    """stations"""
+    data, stations, res = [], set(stations), []
+    for _ in range(int(input())):
+        data.append(loads(input()))
+    while stations:
+        data.sort(key=lambda x: len(set(x["Cities"]).intersection(stations)), reverse=True)
+        current = data.pop(0)
+        use_stations = set(current["Cities"]).intersection(stations)
+        if not use_stations:
+            break
+        res.append(current["Name"])
+        for town in use_stations:
+            stations.remove(town)
+    print(sorted(res))
+radio_stations(loads(input()))
